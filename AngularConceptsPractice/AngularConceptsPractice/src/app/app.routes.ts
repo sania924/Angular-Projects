52f27e88c18c1ac007d5ceb44aa370b8bb891a7c
sania924 <saniayasin924@gmail.com>
2024-06-15 23:23:30 +0500

    forms

--- AngularConceptsPractice/AngularConceptsPractice/src/app/app.routes.ts
+++ AngularConceptsPractice/AngularConceptsPractice/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { IfelseComponent } from './structuralDirective/ifelse/ifelse.component';
 import { NgclassNgstyleComponent } from './structuralDirective/ngclass-ngstyle/ngclass-ngstyle.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 export const routes: Routes = [
   { path: 'ifelse', component: IfelseComponent },
   { path: 'ngclass-ngstyle', component: NgclassNgstyleComponent },
+  { path: 'reactiveform', component: ReactiveFormsModule },
 ];
