74d454b2b223adfccde8d8a1dfdc6313e4551dc1
sania924 <saniayasin924@gmail.com>
2024-08-10 22:16:10 +0500

    angular conceots

--- AngularConceptsPractice/AngularConceptsPractice/src/app/app.component.ts
+++ AngularConceptsPractice/AngularConceptsPractice/src/app/app.component.ts
@@ -1,24 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
-import { TablesComponent } from './primengpractice/tables/tables.component';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './includes/header/header.component';
-import { PrimengpracticeComponent } from './primengpractice/primengpractice/primengpractice.component';
-import { PrimengpracticeheaderComponent } from './includes/primengpracticeheader/primengpracticeheader.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    TablesComponent,
-    CommonModule,
-    RouterLink,
-    HeaderComponent,
-    PrimengpracticeComponent,
-    PrimengpracticeheaderComponent,
-  ],
+  imports: [RouterOutlet, CommonModule, RouterLink, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- AngularConceptsPractice/AngularConceptsPractice/src/app/app.routes.ts
+++ AngularConceptsPractice/AngularConceptsPractice/src/app/app.routes.ts
@@ -7,18 +7,13 @@ import { GetApiComponent } from './Api-integration/get-api/get-api.component';
 import { PostApiComponent } from './Api-integration/post-api/post-api.component';
 import { AlertComponent } from './angularConceptsPractice/alert/alert.component';
 import { ButtonComponent } from './angularConceptsPractice/button/button.component';
-import { PrimengpracticeComponent } from './primengpractice/primengpractice/primengpractice.component';
 import { HeaderComponent } from './includes/header/header.component';
-import { TablesComponent } from './primengpractice/tables/tables.component';
-import { TemplatetableComponent } from './primengpractice/tables/templatetable/templatetable.component';
-import { ResponsivetableComponent } from './primengpractice/tables/responsivetable/responsivetable.component';
 
 import { ViewchildComponent } from './angularConceptsPractice/decorators/viewchild/viewchild.component';
 
 export const routes: Routes = [
   { path: '', component: IfelseComponent },
   { path: 'header', component: HeaderComponent },
-  { path: 'primengpracticeheader', component: PrimengpracticeComponent },
 
   { path: 'ngclass-ngstyle', component: NgclassNgstyleComponent },
   { path: 'templateform', component: TemplateformComponent },
@@ -29,14 +24,6 @@ export const routes: Routes = [
 
   { path: 'alert', component: AlertComponent }, //for @input
   { path: 'button', component: ButtonComponent }, //for @output
-  {
-    path: 'primengpractice',
-    component: PrimengpracticeComponent,
-    children: [
-      { path: 'tables', component: TablesComponent },
-      { path: 'templatetable', component: TemplatetableComponent },
-      { path: 'responsivetable', component: ResponsivetableComponent },
-    ],
-  },
+
   { path: '', redirectTo: '/primengpractice', pathMatch: 'full' }, // Default redirect
 ];
