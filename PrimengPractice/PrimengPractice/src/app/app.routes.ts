502e34939b674c1a8b8e98c3ed8657b27e79b655
sania924 <saniayasin924@gmail.com>
2024-08-26 22:37:44 +0500

    add

--- PrimengPractice/PrimengPractice/src/app/app.routes.ts
+++ PrimengPractice/PrimengPractice/src/app/app.routes.ts
@@ -6,22 +6,13 @@ import { BasicFormComponent } from './FormComponents/basic-form/basic-form.compo
 import { DataviewComponent } from './TableComponents/dataview/dataview.component';
 import { TreetableComponent } from './TableComponents/treetable/treetable.component';
 import { PanelComponent } from './PanelComponents/panel/panel.component';
-import { AdvancePanelComponent } from './PanelComponents/advance-panel/advance-panel.component';
-import { MenubarComponent } from './MenuComponents/menubar/menubar.component';
-import { AdvanceMenuComponent } from './MenuComponents/advance-menu/advance-menu.component';
-import { MiscComponent } from './MiscComponents/misc/misc.component';
 
 export const routes: Routes = [
   { path: 'header', component: HeaderComponent },
   { path: '', component: FilterTableComponent },
   { path: 'dataview', component: DataviewComponent },
-
   { path: 'paginator-table', component: PaginatorTableComponent },
   { path: 'basic-form', component: BasicFormComponent },
   { path: 'treetable', component: TreetableComponent },
   { path: 'panel', component: PanelComponent },
-  { path: 'advance-panel', component: AdvancePanelComponent },
-  { path: 'menubar', component: MenubarComponent },
-  { path: 'advance-menu', component: AdvanceMenuComponent },
-  { path: 'misc', component: MiscComponent },
 ];
