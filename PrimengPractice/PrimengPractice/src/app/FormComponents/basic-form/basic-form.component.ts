eccc3fcf4a6c34464916b9eeb3d4f7f0d8742a89
sania924 <saniayasin924@gmail.com>
2024-08-29 23:38:19 +0500

    update

--- PrimengPractice/PrimengPractice/src/app/FormComponents/basic-form/basic-form.component.ts
+++ PrimengPractice/PrimengPractice/src/app/FormComponents/basic-form/basic-form.component.ts
@@ -48,6 +48,7 @@ export class BasicFormComponent implements OnInit {
 
   // float label
   value: string | undefined;
+  value2: string | undefined;
 
   constructor(private countryService: CountryService) {}
 
