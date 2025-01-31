b59fb367567d1fe2374f3c23535c90035f68ae37
sania924 <saniayasin924@gmail.com>
2025-01-31 11:49:18 +0500

    update

--- BankLoan_App/BankLoan_App/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { LoanApplicationComponent } from './pages/loan-application/loan-application.component';
-import { ApplicationlistComponent } from './pages/applicationlist/applicationlist.component';
-
-export const routes: Routes = [
-  { path: '', redirectTo: 'loanapplication', pathMatch: 'full' },
-  { path: 'loanapplication', component: LoanApplicationComponent },
-  { path: 'applicationlist', component: ApplicationlistComponent },
-];
